8d961de5da6b5f6de7255f97f9bd9a589d959a02
Mazharrais <mazharmajo72@gmail.com>
2024-09-05 13:53:14 +0500

    add code

--- src/components/ToDoApp.jsx
+++ src/components/ToDoApp.jsx
@@ -1,9 +1,22 @@
-import React, { useState } from 'react'
-import { useDispatch } from 'react-redux'
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { FaArrowAltCircleRight } from "react-icons/fa";
+import {addTodo} from '../Redux/Action/action';
 
 const ToDoApp = () => {
   const [todoText, setTodoText] = useState("");
   const dispatch = useDispatch();
+
+   const handleAddTodo = (text) =>{
+      dispatch(addTodo(text));
+   }
+
+   const saveTodo = ()=>{
+    if(todoText.trim() !== ""){
+      handleAddTodo(todoText.trim());
+      setTodoText("")
+    }
+   }
 
   return (
     <div className='max-w-4xl max-auto sm:mt-8 p-4 bg-blue-200 rounded'>
@@ -14,11 +27,13 @@ const ToDoApp = () => {
      
      <div className='flex item-center mb-4'>
      <input type='text' placeholder='Enter your Task'
-         className='flex grow p-2 border-b-2 border-grey-300 rounded focus:outline none focus:border-blue-500'
+         className='flex grow p-2 border-b-2 border-grey-300 rounded focus:outline-none focus:border-blue-500'
            name='text'
            id='addTodo'   
            onChange={(e)=> setTodoText(e.target.value)}  />
-
+          <button className='ml-4 p-2 bg-blue-500 text-white rounded hover:bg-blue
+           focus:outline-none' onClick={saveTodo}>
+            <FaArrowAltCircleRight />Add</button>
 
      </div>
 
@@ -27,4 +42,4 @@ const ToDoApp = () => {
   )
 }
 
-export default ToDoApp
+export default ToDoApp;
