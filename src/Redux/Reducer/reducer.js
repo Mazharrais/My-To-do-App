cb8319074251318af53d3d42e79a4d2a28947903
Mazharrais <mazharmajo72@gmail.com>
2024-09-05 12:51:39 +0500

    Add code

--- src/Redux/Reducer/reducer.js
+++ src/Redux/Reducer/reducer.js
@@ -1,5 +1,6 @@
-import {ADD_TODO} from '../Action/actionTypes';
-import { TOGGLE_TODO } from '../Action/actionTypes';
+import {ADD_TODO, FILTER_TODO, MARK_ALL_COMPLETED_TODO, MARK_COMPLETED_TODO, MARK_INCOMPLETE_TODO,
+     REMOVE_TODO, SEARCH_TODO, TOGGLE_TODO} from '../Action/actionTypes';
+
 
 const initialState = { 
     todos : [],
@@ -26,5 +27,54 @@ const todoReducer = (state = initialState, action) =>{
                 filter: state.filter,
                 search: state.search
             }
+           case REMOVE_TODO:
+            return{
+                todos:state.todos.filter((todo,index)=>index !== action.payload.id),
+                filter: state.filter,
+                search: state.search
+            } 
+          case MARK_COMPLETED_TODO:
+            return{
+                todos:state.todos.map((todo,index)=> index === action.payload.id ?
+                {...todo, completed:true} : todo
+            ),
+            filter: state.filter,
+            search: state.search
+            }
+          case MARK_INCOMPLETE_TODO:
+            return{
+                todos:state.todos.map((todo,index)=> index === action.payload.id ?
+                {...todo, completed:false} : todo
+            ),
+                filter: state.filter,
+                search: state.search
+            }
+          case FILTER_TODO:
+            return{
+                todos: state.todos,
+                filter : action.payload.filter,
+                search: state.search
+            }
+         case SEARCH_TODO:
+            return{
+                todos: state.todos,
+                filter: state.filter,
+                search: action.payload.search
+            } 
+         case MARK_ALL_COMPLETED_TODO:
+            return{
+                todos:state.todos.map((todo)=>
+                ({
+                    ...todo, completed: true
+                })
+                ),
+                filter: state.filter,
+                search: state.search
+            }     
+       default:
+        return state;
+
     }
 }
+
+export default todoReducer;
